5b1d1c70c934a8e9c7f0c8d908ead8e8d7e41f5e
蓝菜 <2118116982@qq.com>
2026-09-05 16:47:57 +0800

    block1: fix RenderGuiEvent DeltaTracker mapping

--- src/main/java/adris/altoclef/eventbus/events/GameOverlayEvent.java
+++ src/main/java/adris/altoclef/eventbus/events/GameOverlayEvent.java
@@ -1,5 +1,6 @@
 package adris.altoclef.eventbus.events;
 
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.DeltaTracker;
 
-public record GameOverlayEvent(GuiGraphics graphics, float partialTick) { }
+public record GameOverlayEvent(GuiGraphics graphics, DeltaTracker partialTick) { }
